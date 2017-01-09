dcb1ab7e98d650c6ca51dfb940e611466d1141b3
Luke Schlangen <lukeschlangen@gmail.com>
2017-01-09 10:23:48 -0600

    Getting new employee data from form

--- client.js
+++ client.js
@@ -1,3 +1,22 @@
 $(function(){
   console.log('jQuery is working!!!!!!!!!');
+  $('#newEmployeeForm').on('submit', function(event){
+    event.preventDefault(); // stop the page from reloading and redirecting
+    console.log('Form has been submitted!!');
+
+    var newEmployeeArray = $(this).serializeArray(); // get the information from the form
+    console.log(newEmployeeArray);
+
+    var newEmployeeObject = {};
+
+    // Loop through all of the input properties in the array
+    // to make a single object
+    for(var i = 0; i < newEmployeeArray.length; i++){
+      var inputFieldName = newEmployeeArray[i].name;
+      var inputFieldValue = newEmployeeArray[i].value;
+      newEmployeeObject[inputFieldName] = inputFieldValue;
+    }
+
+    console.log(newEmployeeObject);
+  });
 });
